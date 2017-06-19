e72db3f86ef511d9540b5fd0b801c0c8bff39050
Sienakasa <Sienakasa@DESKTOP-6TE18F9>
2017-06-19 15:32:07 +0300

    Add project files.

---
+++ Ecommerce/Controllers/UserController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Ecommerce.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Login()
+        {
+            return View("Login");
+        }
+        public ActionResult Register()
+        {
+            return View("Register");
+        }
+        public ActionResult Checkout()
+        {
+            return View("Checkout");
+        }
+    }
+}
